5d64b6388854c97d1a380111f454864b379e9ed3
Maxim Terzhevik <maxt2000@mail.ru>
2022-06-23 15:56:57 +0300

    start fix

--- src/module.js
+++ src/module.js
@@ -16,11 +16,6 @@ export function start() {
   map = L.map("map", {
     scrollWheelZoom: true,
   });
-  L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    maxZoom: 19,
-    attribution:
-      '&copy; OSM Mapnik <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-  }).addTo(map);
 
   // Set the position and zoom level of the map
 
